489a717cfb3e0224d2e9f87d4e1fd769f954e27d
Bonvic Bundi <nyabuyabonvic@gmail.com>
2020-06-03 09:09:05 +0200

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,25 +6,25 @@ import Netflix from '../components/images/netflix';
 
 const project = [
   { 
-    name: 'Property Pro Lite',
+    name: 'Barefoot Nomad',
     id: '1',
+    idea: 'Request trips',
+    image: <Barefoot />,
+    link: 'https://boondocks-bn-frontend-staging.herokuapp.com/',
+  },
+  { 
+    name: 'Property Pro Lite',
+    id: '2',
     idea: 'Real estate',
     image: <Property />,
     link: 'https://property-pro-lite-webpack.herokuapp.com/',
   },
   { 
     name: 'DA Restaurant',
-    id: '2',
+    id: '3',
     idea: 'Restuarant PHP API',
     image: <Restuarant />,
     link: 'https://github.com/b0nbon1/restuarant-php/tree/master/api',
-  },
-  { 
-    name: 'Barefoot Nomad',
-    id: '3',
-    idea: 'Request trips',
-    image: <Barefoot />,
-    link: 'https://boondocks-bn-frontend-staging.herokuapp.com/',
   },
   { 
     name: 'Mini-netflix',
