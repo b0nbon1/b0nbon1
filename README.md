511121e2e63c4c3647de38d39f051bade0d5b6e5
Bonvic Bundi <nyabuyabonvic@gmail.com>
2020-06-26 08:41:00 +0200

    start portfolio v2 from scratch

--- README.md
+++
@@ -1,5 +0,0 @@
-# Bonvic website
-
-## 🚀 Deploy
-
-[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://nyabuya.com)
--- gatsby-browser.js
+++
@@ -1,3 +0,0 @@
-import "./src/styles/site.css";
-import "prismjs/plugins/command-line/prism-command-line.css";
-import "prismjs/plugins/line-numbers/prism-line-numbers.css"
