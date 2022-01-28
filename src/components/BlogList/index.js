397f1f15cd6555742b0981aba07d2179a9706842
bonvic <bundi.bonvic@gmail.com>
2022-01-28 22:00:12 +0300

    CRUD files blog

--- src/components/BlogList/index.js
+++ src/components/BlogList/index.js
@@ -60,7 +60,7 @@ const BlogList = () => {
         }}
         key={post.id}
         >
-          <Link sx={BlogStyle} to={`/blog/${post.slug}`}>{post.frontmatter?.title}</Link>
+          <Link sx={BlogStyle} to={`/writings/${post.slug}`}>{post.frontmatter?.title}</Link>
           <div sx={{ mt: 2 }} />
           <Text sx={{ color: 'var(--theme-ui-colors-textMuted)'}}>{post.frontmatter?.date} &#8212; {post.frontmatter?.tags?.join(", ")}</Text>
         </li>
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -11,7 +11,8 @@ const Footer = () => (
       borderTop: `2px dotted var(--theme-ui-colors-muted)`,
       marginX: 'auto',
       width: ['90%', '80%'],
-      mb: 20,
+      mb: 2,
+      mt: 4,
     }}
   >
     <p>&copy; {new Date().getFullYear()} by Bonvic. All rights reserved.</p>
